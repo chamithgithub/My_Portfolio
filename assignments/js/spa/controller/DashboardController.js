59e26a0d0ee0a2505826524067cce0ad4e0ef22b
Cha1552164 <chamithgithub>
2022-10-13 15:41:35 +0530

    DashboardController item btn on action update

--- assignments/js/spa/controller/DashboardController.js
+++ assignments/js/spa/controller/DashboardController.js
@@ -11,3 +11,10 @@ $('#btn_Customer').click(function () {
     $('#header_container').css('visibility', 'visible');
     $('#customer_main_container').css('visibility', 'visible');
 });
+
+// item btn on action
+$('#btn_Item').click(function () {
+    $('#dashboard_main_container').css('visibility', 'hidden');
+    $('#header_container').css('visibility', 'visible');
+    $('#item_main_container').css('visibility', 'visible');
+});
