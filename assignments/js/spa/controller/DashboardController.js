b3c51c3b54539879059ea0453b349e0b9b12c0a1
Cha1552164 <chamithgithub>
2022-10-13 15:07:48 +0530

    controller cust btn coding

--- assignments/js/spa/controller/DashboardController.js
+++ assignments/js/spa/controller/DashboardController.js
@@ -4,4 +4,9 @@ $('#header_container').css('visibility', 'hidden');
 $('#customer_main_container').css('visibility', 'hidden');
 
 
-
+// customer btn on action
+$('#btn_Customer').click(function () {
+    $('#dashboard_main_container').css('visibility', 'hidden');
+    $('#header_container').css('visibility', 'visible');
+    $('#customer_main_container').css('visibility', 'visible');
+});
